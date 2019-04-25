f9336cb62b942826227d5dbad10b490006bea836
Frank Diaz <diazfrankp@gmail.com>
2019-04-25 17:00:16 -0400

    ChangedToPeopleIds

--- Properties/ViewModels/DeleteContactsViewModel.cs
+++ Properties/ViewModels/DeleteContactsViewModel.cs
@@ -4,6 +4,6 @@ namespace PhoneBookBackEnd.ViewModels
 {
   public class DeleteContactsViewModel
   {
-    public List<int> ContactIds { get; set; } = new List<int>();
+    public List<int> PeopleIds { get; set; } = new List<int>();
   }
 }
